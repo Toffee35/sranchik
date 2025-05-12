16f79c5c1d912977f4d25bdceb93a813c42f77b9
Toffee35 <nailzagru@gmail.com>
2025-05-12 22:51:43 +0300

    proto

--- src/routes/messages/judging.py
+++ src/routes/messages/judging.py
@@ -5,13 +5,16 @@ from aiogram.types import Message
 from src.keyboards.reply import judging_choise
 from src.states import Base
 from src.storages import users
+
 judging = Router()
 
 
 @judging.message(F.text == "Оценка")
 async def _judging(message: Message, state: FSMContext):
     if not len(users) > 1:
-        await message.answer(html.italic("Пока что ты единственный кого можно оценить..."))
+        await message.answer(
+            html.italic("Пока что ты единственный кого можно оценить...")
+        )
         return
 
     await state.set_state(Base.Judging)
